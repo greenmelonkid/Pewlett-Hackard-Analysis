652295f907932e5f77ad5b7545f7cca1d192da16
greenmelonkid <lag8675@gmail.com>
2022-03-28 20:34:57 -0500

    message

--- schema.sql
+++ schema.sql
@@ -16,13 +16,16 @@ CREATE TABLE employees (
 	PRIMARY KEY (emp_no)
 );
 
+DROP TABLE employees; 
+
+
 CREATE TABLE dept_managers (
 dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
 
@@ -31,7 +34,7 @@ CREATE TABLE salaries(
 	salary INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );	
 
@@ -39,20 +42,26 @@ CREATE TABLE dept_emp(
 	dept_no VARCHAR NOT NULL,
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
-	PRIMARY KEY (dept_no)
+	to_date DATE NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+	to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
 SELECT * FROM departments;
+
+
+
+
+
+
 
